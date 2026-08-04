0b652d22625f0dcbcafe6a71b9e8c38b8f1a6ffd
yjaabari21 <yousef33jaabari@gmail.com>
2026-08-04 23:47:00 +0300

    Improve Videos in Stories

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
     }
 
     signingConfigs {
